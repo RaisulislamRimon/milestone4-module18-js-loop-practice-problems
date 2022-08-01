e93c58445fc6dd0b5686154f282e8ad8776329b2
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-01 18:46:22 +0000

    array loop

---
+++ js-loops-practice-problems/7 array-loop.js
@@ -0,0 +1,23 @@
+/*
+var arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+for (var i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+ */
+
+/* 
+var arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+for (var i = 0; i < arr.length; i++) {
+  ar = arr[i];
+  console.log(ar);
+}
+ */
+var items = ["bottle", "sunglass", "pen", "pencil", "book", "eraser"];
+/* 
+for (var i = 0; i < items.length; i++) {
+  console.log(items[i]);
+} */
+for (var i = 0; i < items.length; i++) {
+  var item = items[i];
+  console.log(item);
+}
