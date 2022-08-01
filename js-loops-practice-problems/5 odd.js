a9744068efc2e3a4c0da2648fffd0aa095deb09d
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-01 18:04:56 +0000

    odd

---
+++ js-loops-practice-problems/5 odd.js
@@ -0,0 +1,16 @@
+/* 
+var number = 1;
+while (number < 10) {
+  if (number % 2 !== 0) {
+    console.log("odd", number);
+  }
+  number++;
+}
+*/
+
+var number = 1;
+// while (number <= 10) {
+while (number <= 20) {
+  console.log(number);
+  number += 2;
+}
