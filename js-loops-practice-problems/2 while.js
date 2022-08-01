c33157c833b2c4a262664e7eb137e8eac3c439a2
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-01 17:35:42 +0000

    while loop

---
+++ js-loops-practice-problems/2 while.js
@@ -0,0 +1,19 @@
+// while loop
+var roastGiven = 0;
+
+/* 
+if (condition) {
+  // do something
+} */
+/* 
+if (roastGiven < 7) {
+  // do something
+}
+ */
+while (roastGiven < 7) {
+  console.log("Roast den, please!!");
+  //   roastGiven = roastGiven + 1;
+  //   roastGiven += 1;
+  console.log(roastGiven);
+  roastGiven++;
+}
