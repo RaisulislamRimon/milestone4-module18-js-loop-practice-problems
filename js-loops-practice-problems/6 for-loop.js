58e4037190d27b3a63346252b59c7d92cbee2764
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-01 18:25:57 +0000

    for loop

---
+++ js-loops-practice-problems/6 for-loop.js
@@ -0,0 +1,40 @@
+/* 
+var roastGiven = 0;
+while (roastGiven < 7) {
+  console.log("roast den, please!!");
+  roastGiven++;
+}
+ */
+
+// for loop
+/* for (var roastGiven = 0; roastGiven < 7; roastGiven++) {
+  console.log("roast den, please!!");
+} */
+
+// simple version of for loop
+/* 
+console.log("simple for loop");
+for (let i = 0; i < 7; i++) {
+  //   console.log("roast den, please!!");
+  console.log(i);
+}
+ */
+
+/* 
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+ */
+/* 
+for (var i = 1; i <= 10; i++) {
+  console.log(i);
+}
+ */
+/* 
+for (var i = 0; i <= 20; i += 2) {
+  console.log(i);
+}
+ */
+for (var i = 1; i <= 20; i += 2) {
+  console.log(i);
+}
