5356bd2bc97cd6a8196b773ff374b012c1261e78
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-01 17:58:51 +0000

    even

---
+++ js-loops-practice-problems/4 even.js
@@ -0,0 +1,13 @@
+// var number = 1;
+var number = 0;
+while (number <= 10) {
+  if (number % 2 === 0) {
+    console.log("even");
+  } else {
+    console.log("odd");
+  }
+  console.log(number);
+  //   number++;
+  //   number += 1;
+  number += 2;
+}
