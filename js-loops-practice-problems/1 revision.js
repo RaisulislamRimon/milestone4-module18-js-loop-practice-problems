cc4a72ec89d76093c86dc5338514ece02122cdd6
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-01 23:02:12 +0600

    Add files via upload

---
+++ js-loops-practice-problems/1 revision.js
@@ -0,0 +1,31 @@
+// string
+var bottle = 'water';
+var bottleColor = 'yellow';
+var penColor = 'blue';
+
+//number type variable
+var bottlePrice = 500;
+var sunglassPrice = 120;
+
+//Boolean type variable
+var isExpensive = true;
+
+//array
+//var itemsOnTable = [10, 20, 30, 40, 50];
+var itemsOnTable = ['bottle', 'pen', 'mouse', 'sunglass', 'paper'];
+
+var item3 = itemsOnTable[3];
+itemsOnTable[4] = 'nootbook';
+
+var penIndex = itemsOnTable.indexOf('pen');
+
+// push, pop
+
+// conditionals
+if(itemsOnTable.length > 4) {
+    console.log("found");
+} else if(itemsOnTable[3] == 'pen') {
+    console.log("pen found");
+} else {
+    console.log("nothing found");
+}
