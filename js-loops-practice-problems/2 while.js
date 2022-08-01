c7f696cf2bb3d3d9797db06a733e44a252de2016
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-01 17:53:12 +0000

    numbers

--- js-loops-practice-problems/2 while.js
+++ js-loops-practice-problems/2 while.js
@@ -1,4 +1,12 @@
 // while loop
+
+/* 
+1. loop variable
+2. condition inside while loop
+3. loop body
+4. change the loop variable
+
+*/
 var roastGiven = 0;
 
 /* 
---
+++ js-loops-practice-problems/3 numbers.js
@@ -0,0 +1,13 @@
+/* 
+var number = 0;
+while (number < 10) {
+  console.log(number);
+  number++; // number = number + 1
+}
+ */
+
+var number = 1;
+while (number <= 20) {
+  console.log(number);
+  number++; // number = number + 1
+}
