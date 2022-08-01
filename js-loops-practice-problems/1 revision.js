5ae74cb8f8da41ee42517fd50d6335bbfd0d14df
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-01 17:12:50 +0000

    update

--- js-loops-practice-problems/1 revision.js
+++ js-loops-practice-problems/1 revision.js
@@ -1,7 +1,7 @@
-// string
-var bottle = 'water';
-var bottleColor = 'yellow';
-var penColor = 'blue';
+// string type variable
+var bottle = "water";
+var bottleColor = "yellow";
+var penColor = "blue";
 
 //number type variable
 var bottlePrice = 500;
@@ -10,22 +10,25 @@ var sunglassPrice = 120;
 //Boolean type variable
 var isExpensive = true;
 
+// checking the javascript variable data type
+console.log(typeof isExpensive);
+
 //array
 //var itemsOnTable = [10, 20, 30, 40, 50];
-var itemsOnTable = ['bottle', 'pen', 'mouse', 'sunglass', 'paper'];
+var itemsOnTable = ["bottle", "pen", "mouse", "sunglass", "paper"];
 
 var item3 = itemsOnTable[3];
-itemsOnTable[4] = 'nootbook';
+itemsOnTable[4] = "nootbook";
 
-var penIndex = itemsOnTable.indexOf('pen');
+var penIndex = itemsOnTable.indexOf("pen");
 
 // push, pop
 
 // conditionals
-if(itemsOnTable.length > 4) {
-    console.log("found");
-} else if(itemsOnTable[3] == 'pen') {
-    console.log("pen found");
+if (itemsOnTable.length > 4) {
+  console.log("found");
+} else if (itemsOnTable[3] == "pen") {
+  console.log("pen found");
 } else {
-    console.log("nothing found");
+  console.log("nothing found");
 }
